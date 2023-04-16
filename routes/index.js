46a0364d42480260f223efbe5e330788c6878dc6
Maxim Bernatsky <maxsateon@yandex.ru>
2023-04-16 12:35:18 +0300

    edit .gitignore

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,12 @@ const movieRouter = require('./movies');
 const { auth } = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
 const NotFoundError = require('../errors/NotFoundError');
-const regexUrl = require('../utils/validation');
 
 router.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(regexUrl),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
